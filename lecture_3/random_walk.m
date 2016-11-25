afd53a98c49c76d241ac65e15e40248b17a229f1
Alex Nichol <unixpickle@gmail.com>
2016-11-25 17:23:48 -0500

    fixed random walk for lecture 3
    My markov matrix was setup incorrectly, thus yielding bad results.

--- lecture_3/random_walk.m
+++ lecture_3/random_walk.m
@@ -1,29 +1,41 @@
-transitionMatrix = eye(14) * 0;
-idx = -1;
+transitionMatrix = eye(16) * 0;
+idx = 0;
 for i = 1:4
   for j = 1:4
     idx++;
-    if i == 1 && j == 1
-      continue
-    elseif i == 4 && j == 4
+    if (i == 1 && j == 1) || (i == 4 && j == 4)
+      transitionMatrix(idx,idx) = 1;
       continue
     end
-    if j > 1 && idx != 1
+    if j > 1
       transitionMatrix(idx,idx-1) = 0.25;
+    else
+      transitionMatrix(idx,idx) += 0.25;
     end
-    if i > 1 && idx != 4
+    if i > 1
       transitionMatrix(idx,idx-4) = 0.25;
+    else
+      transitionMatrix(idx,idx) += 0.25;
     end
-    if j < 4 && idx != 14
+    if j < 4
       transitionMatrix(idx,idx+1) = 0.25;
+    else
+      transitionMatrix(idx,idx) += 0.25;
     end
-    if i < 4 && idx != 11
+    if i < 4
       transitionMatrix(idx,idx+4) = 0.25;
+    else
+      transitionMatrix(idx,idx) += 0.25;
     end
   end
 end
-solution = (eye(14,14) - transitionMatrix)\repmat([-1], 14, 1);
-[0 solution(1) solution(2) solution(3);
-  solution(4) solution(5) solution(6) solution(7);
-  solution(8) solution(9) solution(10) solution(11);
-  solution(12) solution(13) solution(14) 0]
+
+immediate = repmat([-1], 14, 1);
+solution = [0; (eye(14,14) - transitionMatrix(2:15,2:15)) \ immediate; 0];
+resMat = eye(4,4)*0;
+for i = 1:4
+  for j = 1:4
+    resMat(i,j) = solution(j+4*(i-1));
+  end
+end
+resMat
